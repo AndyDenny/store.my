c262758852163868aa474ed79d3112bcd6fa31fd
AndyDenny <tp0yah@gmail.com>
2023-10-29 23:35:48 +0300

    add first settings

--- config.php
+++ config.php
@@ -1,1 +1,10 @@
 <?php
+defined('VG_ACCESS') or die('Access denied');
+
+const SITE_URL = 'http:store.my';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASSWORD = '';
+const DB_NAME = 'store_db';
