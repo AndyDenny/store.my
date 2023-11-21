573fc4c460c0c3f0eb81d232df85423c04e26310
AndyDenny <tp0yah@gmail.com>
2023-11-21 23:00:11 +0300

    Add render method in BaseController

--- core/base/controller/BaseController.php
+++ core/base/controller/BaseController.php
@@ -9,6 +9,9 @@ use core\base\exceptions\RouteException;
 abstract class BaseController
 {
 
+    protected $page;
+    protected $errors;
+
     protected $controller;
     protected $inputMethod;
     protected $outputMethod;
@@ -33,13 +36,44 @@ abstract class BaseController
 
         }
         catch (\ReflectionException $e){
-            throw new RouteException($e);
+            throw new RouteException($e->getMessage());
         }
 
     }
 
     public function request($args){
+        $this->parameters = $args['parameters'];
+        $inputData = $args['inputMethod'];
+        $outputData = $args['outputMethod'];
 
+        $this->$inputData();
+        $this->page = $this->$outputData();
+
+        if($this->errors){
+            $this->writeLog();
+        }
+
+        $this->getPage();
+
+    }
+
+    protected function render($path='',$parameters=[]){
+
+        extract($parameters);
+        if(!$path){
+            $path = TEMPLATE . explode('controller',strtolower((new \ReflectionClass($this))->getShortName()))[0];
+        }
+
+        ob_start();
+
+        if(!@include_once $path . '.php') throw new RouteException('Отсутствует шаблон - ' .$path);
+
+        return ob_get_clean();
+
+    }
+
+    protected function getPage(){
+        exit($this->page);
     }
 
 }
